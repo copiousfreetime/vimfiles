92f7b65c55980a7317adeec3a3cdceab1632c3e0
Jeremy Hinegardner <jhinegardner@easypost.com>
2025-09-12 17:00:56 -0500

    fix easy-align

--- lua/plugins/easyalign.lua
+++ lua/plugins/easyalign.lua
@@ -1,14 +1,17 @@
 return {
-  "junegunn/vim-easy-align",
-  keys = {
-    -- Start interactive EasyAlign in visual mode
-    { "<Enter>", "<Plug>(EasyAlign)", mode = "v", desc = "EasyAlign" },
+  {
+    "junegunn/vim-easy-align",
+    event = "VeryLazy",
+    keys = {
+      -- Start interactive EasyAlign in visual mode
+      { "<Enter>", "<Plug>(EasyAlign)", mode = "v", desc = "EasyAlign" },
 
-    -- Start interactive EasyAlign with a Vim movement
-    { "<leader>a", "<Plug>(EasyAlign)", mode = "n", desc = "EasyAlign with motion" },
+      -- Start interactive EasyAlign with a Vim movement
+      { "<leader>a", "<Plug>(EasyAlign)", mode = "n", desc = "EasyAlign with motion" },
 
-    -- Live EasyAlign
-    { "<leader><Enter>", "<Plug>(LiveEasyAlign)", mode = "v", desc = "Live EasyAlign" },
-    { "<leader><leader>a", "<Plug>(LiveEasyAlign)", mode = "n", desc = "Live EasyAlign with motion" },
+      -- Live EasyAlign
+      { "<leader><Enter>", "<Plug>(LiveEasyAlign)", mode = "v", desc = "Live EasyAlign" },
+      { "<leader><leader>a", "<Plug>(LiveEasyAlign)", mode = "n", desc = "Live EasyAlign with motion" },
+    },
   },
 }
