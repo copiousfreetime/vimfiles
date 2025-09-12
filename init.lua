b3a358edb111b3d1a5e7d75f60914f6f0924451c
Jeremy Hinegardner <jhinegardner@easypost.com>
2025-09-12 16:26:45 -0500

    adding crystal langugage

--- init.lua
+++ init.lua
@@ -7,10 +7,7 @@ require('config.options')
 require('config.keymaps')
 require('config.autocmds')
 require('config.colorscheme')
-
--- Load custom statusline (optional - lightline.vim is also configured)
--- Uncomment the line below if you prefer the custom statusline over lightline
--- require('config.statusline').setup()
+require('config.crystal')
 
 -- Print confirmation that Lua config is loaded
 vim.notify("Lua configuration loaded successfully!", vim.log.levels.INFO)
