ccb116840911e062e8b09c07b1116271a5b36b03
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2026-04-06 23:49:31 -0600

    rework to latest tree-sitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,72 +1,80 @@
-local ensure_installed = {
-  "bash",
-  "c",
-  "css",
-  "csv",
-  "dart",
-  "diff",
-  "elixir",
-  "elm",
-  "erlang",
-  "git_config",
-  "gitcommit",
-  "gitignore",
-  "go",
-  "html",
-  "javascript",
-  "jq",
-  "json",
-  "json5",
-  "lua",
-  "make",
-  "markdown",
-  "markdown_inline",
-  "mermaid",
-  "perl",
-  "php",
-  "python",
-  "query",
-  "ruby",
-  "regex",
-  "sql",
-  "ssh_config",
-  "terraform",
-  "tmux",
-  "toml",
-  "tsx",
-  "typescript",
-  "typespec",
-  "vim",
-  "xml",
-  "yaml",
-}
-
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = 'main',
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter").setup()
+  "nvim-treesitter/nvim-treesitter",
+  branch = 'main',
+  lazy = false,
+  build = ":TSUpdate",
+  config = function ()
 
-      -- Install any missing parsers from ensure_installed list
-      local installed = require("nvim-treesitter").get_installed()
-      local installed_set = {}
-      for _, lang in ipairs(installed) do
-        installed_set[lang] = true
-      end
+    local ts = require('nvim-treesitter')
+    local languages = {
+      "bash",
+      "c",
+      "crystal",
+      "css",
+      "csv",
+      "dart",
+      "diff",
+      "elixir",
+      "elm",
+      "erlang",
+      "git_config",
+      "gitcommit",
+      "gitignore",
+      "go",
+      "html",
+      "javascript",
+      "jq",
+      "json",
+      "json5",
+      "lua",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "mermaid",
+      "perl",
+      "php",
+      "python",
+      "query",
+      "ruby",
+      "regex",
+      "sql",
+      "ssh_config",
+      "terraform",
+      "tmux",
+      "toml",
+      "tsx",
+      "typescript",
+      "typespec",
+      "vim",
+      "xml",
+      "yaml",
+    }
 
-      local missing = {}
-      for _, lang in ipairs(ensure_installed) do
-        if not installed_set[lang] then
-          table.insert(missing, lang)
-        end
+    vim.api.nvim_create_autocmd('User', {
+      pattern   = 'TSUpdate',
+      callback  = function ()
+        require('nvim-treesitter.parsers').crystal = {
+          install_info = {
+            url                 = 'https://github.com/crystal-lang-tools/tree-sitter-crystal',
+            generate            = false,
+            generate_from_json  = false,
+            queries             = 'queries/nvim'
+          }
+        }
       end
+    })
+    vim.treesitter.language.register('crystal', { 'cr' })
 
-      if #missing > 0 then
-        require("nvim-treesitter").install(missing)
+    ts.setup()
+    ts.install(languages)
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = languages,
+      callback = function ()
+        vim.treesitter.start()
+
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end
-    end,
-  },
+    })
+  end
 }
