c279366b0726ae5ca7889be494d25fc4dd2de0c1
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2026-04-06 23:40:30 -0600

    switch to tree-sitter main

--- init.lua
+++ init.lua
@@ -7,7 +7,6 @@ require('config.options')
 require('config.keymaps')
 require('config.autocmds')
 require('config.colorscheme')
-require('config.crystal')
 
 -- Print confirmation that Lua config is loaded
 vim.notify("Lua configuration loaded successfully!", vim.log.levels.INFO)
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,59 +1,72 @@
+local ensure_installed = {
+  "bash",
+  "c",
+  "css",
+  "csv",
+  "dart",
+  "diff",
+  "elixir",
+  "elm",
+  "erlang",
+  "git_config",
+  "gitcommit",
+  "gitignore",
+  "go",
+  "html",
+  "javascript",
+  "jq",
+  "json",
+  "json5",
+  "lua",
+  "make",
+  "markdown",
+  "markdown_inline",
+  "mermaid",
+  "perl",
+  "php",
+  "python",
+  "query",
+  "ruby",
+  "regex",
+  "sql",
+  "ssh_config",
+  "terraform",
+  "tmux",
+  "toml",
+  "tsx",
+  "typescript",
+  "typespec",
+  "vim",
+  "xml",
+  "yaml",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = { "VeryLazy" },
-    branch = 'master',
+    branch = 'main',
     lazy = false,
     build = ":TSUpdate",
-    opts = {
-      highlight = { enable = true },
-      indent = { enable = true },
-      auto_install = true,
-      ensure_installed = {
-        "bash",
-        "c",
-        "crystal",
-        "css",
-        "csv",
-        "dart",
-        "diff",
-        "elixer",
-        "elm",
-        "erlang",
-        "git_config",
-        "gitcommit",
-        "gitignore",
-        "go",
-        "html",
-        "javascript",
-        "jq",
-        "json",
-        "json5",
-        "latex",
-        "lua",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "mermaid",
-        "perl",
-        "php",
-        "python",
-        "query",
-        "ruby",
-        "regex",
-        "sql",
-        "ssh_config",
-        "terraform",
-        "tmux",
-        "toml",
-        "tsx",
-        "typescript",
-        "typespec",
-        "vim",
-        "xml",
-        "yaml",
-      },
-    },
-  },
+    config = function()
+      require("nvim-treesitter").setup()
 
+      -- Install any missing parsers from ensure_installed list
+      local installed = require("nvim-treesitter").get_installed()
+      local installed_set = {}
+      for _, lang in ipairs(installed) do
+        installed_set[lang] = true
+      end
+
+      local missing = {}
+      for _, lang in ipairs(ensure_installed) do
+        if not installed_set[lang] then
+          table.insert(missing, lang)
+        end
+      end
+
+      if #missing > 0 then
+        require("nvim-treesitter").install(missing)
+      end
+    end,
+  },
 }
